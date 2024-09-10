5f2dae102bd18dad031793e5ff35dff893f02ce6
steciuk <a.t.steciuk@gmail.com>
2024-09-10 11:02:10 +0200

    remove boilerplate and test libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: ` <h1>{{ title }}</h1> `,
 })
 export class AppComponent {
-  title = 'recruitment-task-elements';
+  title = 'Hello, World';
 }
