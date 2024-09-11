e86aa7e4d5fc37dadc81a4f918b71bc8262d1c50
steciuk <a.t.steciuk@gmail.com>
2024-09-11 13:04:50 +0200

    elements name filtering

---
+++ src/app/components/clearable-input.component.ts
@@ -0,0 +1,57 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-clearable-input',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  template: `
+    <mat-form-field>
+      <mat-label>{{ label }}</mat-label>
+      <input
+        matInput
+        type="text"
+        [(ngModel)]="value"
+        (ngModelChange)="onValueChange($event)"
+      />
+      @if (value) {
+      <button matSuffix mat-icon-button aria-label="Clear" (click)="clear()">
+        <mat-icon>close</mat-icon>
+      </button>
+      }
+    </mat-form-field>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ClearableInputComponent {
+  @Input() label = 'Input';
+  @Input() initValue = '';
+  @Output() readonly valueChange = new EventEmitter<string>();
+
+  protected value = this.initValue;
+
+  protected onValueChange(value: string) {
+    this.valueChange.emit(value);
+  }
+
+  protected clear() {
+    this.value = '';
+    this.valueChange.emit('');
+  }
+}
--- src/app/components/elements-table.component.ts
+++ src/app/components/elements-table.component.ts
@@ -2,51 +2,86 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { BaseComponent } from './base.component';
 import { ElementsService } from '../services/elements.service';
-import { CommonModule } from '@angular/common';
 import { PeriodicElement } from '../types/PeriodicElement';
+import { ClearableInputComponent } from './clearable-input.component';
+import {
+  BehaviorSubject,
+  combineLatestWith,
+  debounceTime,
+  map,
+  startWith,
+} from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+const FILTER_DEBOUNCE_TIME = 2000;
 
 @Component({
   selector: 'app-elements-table',
   standalone: true,
-  imports: [MatTableModule, CommonModule],
-  template: ` <table mat-table [dataSource]="dataSource$">
-    <!-- Position Column -->
-    <ng-container matColumnDef="number">
-      <th mat-header-cell *matHeaderCellDef>Number</th>
-      <td mat-cell *matCellDef="let element">{{ element.number }}</td>
-    </ng-container>
+  imports: [MatTableModule, ClearableInputComponent, CommonModule],
+  template: ` <app-clearable-input
+      label="Filter"
+      (valueChange)="onFilterValueChange($event)"
+    />
+    <table mat-table [dataSource]="dataSource$">
+      <!-- Position Column -->
+      <ng-container matColumnDef="number">
+        <th mat-header-cell *matHeaderCellDef>Number</th>
+        <td mat-cell *matCellDef="let element">{{ element.number }}</td>
+      </ng-container>
 
-    <!-- Name Column -->
-    <ng-container matColumnDef="name">
-      <th mat-header-cell *matHeaderCellDef>Name</th>
-      <td mat-cell *matCellDef="let element">{{ element.name }}</td>
-    </ng-container>
+      <!-- Name Column -->
+      <ng-container matColumnDef="name">
+        <th mat-header-cell *matHeaderCellDef>Name</th>
+        <td mat-cell *matCellDef="let element">{{ element.name }}</td>
+      </ng-container>
 
-    <!-- Symbol Column -->
-    <ng-container matColumnDef="symbol">
-      <th mat-header-cell *matHeaderCellDef>Symbol</th>
-      <td mat-cell *matCellDef="let element">{{ element.symbol }}</td>
-    </ng-container>
+      <!-- Symbol Column -->
+      <ng-container matColumnDef="symbol">
+        <th mat-header-cell *matHeaderCellDef>Symbol</th>
+        <td mat-cell *matCellDef="let element">{{ element.symbol }}</td>
+      </ng-container>
 
-    <!-- Weight Column -->
-    <ng-container matColumnDef="atomic_mass">
-      <th mat-header-cell *matHeaderCellDef>Atomic mass</th>
-      <td mat-cell *matCellDef="let element">{{ element.atomic_mass }}</td>
-    </ng-container>
+      <!-- Weight Column -->
+      <ng-container matColumnDef="atomic_mass">
+        <th mat-header-cell *matHeaderCellDef>Atomic mass</th>
+        <td mat-cell *matCellDef="let element">{{ element.atomic_mass }}</td>
+      </ng-container>
 
-    <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-    <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
-  </table>`,
+      <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+      <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
+    </table>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ElementsTableComponent extends BaseComponent {
   private readonly elementsService = inject(ElementsService);
-  protected dataSource$ = this.elementsService.get$();
 
-  protected displayedColumns: (keyof PeriodicElement)[] = [
+  protected readonly filterValue$ = new BehaviorSubject('');
+  protected readonly displayedColumns: (keyof PeriodicElement)[] = [
     'number',
     'name',
     'symbol',
     'atomic_mass',
   ];
+  protected readonly dataSource$ = this.elementsService.get$().pipe(
+    combineLatestWith(
+      this.filterValue$.pipe(
+        debounceTime(FILTER_DEBOUNCE_TIME),
+        // Do not wait for the first value to start filtering
+        startWith('')
+      )
+    ),
+    map(([elements, filterValue]) => {
+      if (filterValue) {
+        return elements.filter((element) =>
+          element.name.toLowerCase().includes(filterValue.toLowerCase())
+        );
+      }
+      return elements;
+    })
+  );
+
+  protected onFilterValueChange(value: string) {
+    this.filterValue$.next(value);
+  }
 }
