85ada33a98915d5dd24c14c0d6d54d36ded09acc
steciuk <a.t.steciuk@gmail.com>
2024-09-17 14:28:47 +0200

    add sorting to table view

--- src/app/components/elements-table.component.ts
+++ src/app/components/elements-table.component.ts
@@ -1,15 +1,22 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { PeriodicElement } from '../types/PeriodicElement';
-import { map, Observable } from 'rxjs';
+import { combineLatestWith, map, Observable } from 'rxjs';
 import { ElementMarkValueMatchComponent } from './element-mark-value-match.component';
 import { LoadingComponent } from './loading.component';
 import {
   ElementsViewComponent,
-  PeriodicElementWIthMatches,
+  PeriodicElementWIthMatches as PeriodicElementWithMatches,
 } from './elements-view.component';
 import { FindMatchesService } from '../services/find-matches.service';
 import { RxLet } from '@rx-angular/template/let';
+import { MatSortModule, Sort } from '@angular/material/sort';
+import { rxState } from '@rx-angular/state';
+
+export type SortState = {
+  active: keyof PeriodicElement;
+  direction: 'asc' | 'desc';
+};
 
 @Component({
   selector: 'app-elements-table',
@@ -19,15 +26,26 @@ import { RxLet } from '@rx-angular/template/let';
     ElementMarkValueMatchComponent,
     LoadingComponent,
     RxLet,
+    MatSortModule,
   ],
   host: { class: 'block overflow-x-auto' },
   template: `
     <ng-container
       *rxLet="elementsWithMatches$; let elementsWithMatches; suspense: loading"
     >
-      <mat-table [dataSource]="elementsWithMatches" class="min-w-[650px]">
+      <mat-table
+        [dataSource]="elementsWithMatches"
+        matSort
+        (matSortChange)="onSortChange($event)"
+        class="min-w-[650px]"
+      >
         <ng-container matColumnDef="number">
-          <mat-header-cell *matHeaderCellDef>Number</mat-header-cell>
+          <mat-header-cell
+            *matHeaderCellDef
+            mat-sort-header
+            sortActionDescription="Sort by number"
+            >Number</mat-header-cell
+          >
           <mat-cell *matCellDef="let elementWithMatches">
             <app-element-mark-value-match
               [value]="elementWithMatches.element.number"
@@ -37,7 +55,12 @@ import { RxLet } from '@rx-angular/template/let';
         </ng-container>
 
         <ng-container matColumnDef="name">
-          <mat-header-cell *matHeaderCellDef>Name</mat-header-cell>
+          <mat-header-cell
+            *matHeaderCellDef
+            mat-sort-header
+            sortActionDescription="Sort by name"
+            >Name</mat-header-cell
+          >
           <mat-cell *matCellDef="let elementWithMatches">
             <app-element-mark-value-match
               [value]="elementWithMatches.element.name"
@@ -47,7 +70,12 @@ import { RxLet } from '@rx-angular/template/let';
         </ng-container>
 
         <ng-container matColumnDef="symbol">
-          <mat-header-cell *matHeaderCellDef>Symbol</mat-header-cell>
+          <mat-header-cell
+            *matHeaderCellDef
+            mat-sort-header
+            sortActionDescription="Sort by symbol"
+            >Symbol</mat-header-cell
+          >
           <mat-cell *matCellDef="let elementWithMatches">
             <app-element-mark-value-match
               [value]="elementWithMatches.element.symbol"
@@ -57,7 +85,12 @@ import { RxLet } from '@rx-angular/template/let';
         </ng-container>
 
         <ng-container matColumnDef="category">
-          <mat-header-cell *matHeaderCellDef>Category</mat-header-cell>
+          <mat-header-cell
+            *matHeaderCellDef
+            mat-sort-header
+            sortActionDescription="Sort by category"
+            >Category</mat-header-cell
+          >
           <mat-cell *matCellDef="let elementWithMatches">
             <app-element-mark-value-match
               [value]="elementWithMatches.element.category"
@@ -67,7 +100,12 @@ import { RxLet } from '@rx-angular/template/let';
         </ng-container>
 
         <ng-container matColumnDef="phase">
-          <mat-header-cell *matHeaderCellDef>Phase</mat-header-cell>
+          <mat-header-cell
+            *matHeaderCellDef
+            mat-sort-header
+            sortActionDescription="Sort by phase"
+            >Phase</mat-header-cell
+          >
           <mat-cell *matCellDef="let elementWithMatches">
             <app-element-mark-value-match
               [value]="elementWithMatches.element.phase"
@@ -77,7 +115,12 @@ import { RxLet } from '@rx-angular/template/let';
         </ng-container>
 
         <ng-container matColumnDef="atomic_mass">
-          <mat-header-cell *matHeaderCellDef>Atomic mass</mat-header-cell>
+          <mat-header-cell
+            *matHeaderCellDef
+            mat-sort-header
+            sortActionDescription="Sort by atomic mass"
+            >Atomic mass</mat-header-cell
+          >
           <mat-cell *matCellDef="let elementWithMatches">
             <app-element-mark-value-match
               [value]="elementWithMatches.element.atomic_mass"
@@ -105,6 +148,10 @@ import { RxLet } from '@rx-angular/template/let';
 export class ElementsTableComponent extends ElementsViewComponent {
   private readonly findMatchesService = inject(FindMatchesService);
 
+  private readonly state = rxState<{
+    sorting: SortState | null;
+  }>(({ set }) => set({ sorting: null }));
+
   protected readonly USED_KEYS = [
     'number',
     'name',
@@ -115,7 +162,7 @@ export class ElementsTableComponent extends ElementsViewComponent {
   ] as const satisfies (keyof PeriodicElement)[];
 
   protected readonly elementsWithMatches$: Observable<
-    PeriodicElementWIthMatches[]
+    PeriodicElementWithMatches[]
   > = this.elementsWithFilter$.pipe(
     map(([elements, filterValue]) => {
       const elementsWithMatches = elements.map((element) => ({
@@ -134,5 +181,65 @@ export class ElementsTableComponent extends ElementsViewComponent {
               Object.keys(elementWithMatches.filterMatches).length > 0,
           );
     }),
+    combineLatestWith(this.state.select('sorting')),
+    map(([elementsWithMatches, sorting]) => {
+      if (!sorting) {
+        return elementsWithMatches;
+      }
+
+      return this.sortElementsWithMatches(elementsWithMatches, sorting);
+    }),
   );
+
+  protected onSortChange($event: Sort) {
+    if ($event.direction === '') {
+      this.state.set({ sorting: null });
+    } else {
+      this.state.set({ sorting: $event as SortState });
+    }
+  }
+
+  private sortElementsWithMatches(
+    elementsWithMatches: PeriodicElementWithMatches[],
+    sorting: SortState,
+  ): PeriodicElementWithMatches[] {
+    const { active, direction } = sorting;
+
+    return elementsWithMatches.slice().sort((a, b) => {
+      const valueA = a.element[active];
+      const valueB = b.element[active];
+
+      if (
+        valueA === null ||
+        valueB === null ||
+        valueA === undefined ||
+        valueB === undefined
+      ) {
+        if (
+          (valueA === null || valueA === undefined) &&
+          (valueB === null || valueB === undefined)
+        ) {
+          return 0;
+        }
+
+        if (valueA === null || valueA === undefined) {
+          return direction === 'asc' ? -1 : 1;
+        }
+
+        return direction === 'asc' ? 1 : -1;
+      }
+
+      if (typeof valueA === 'string' && typeof valueB === 'string') {
+        return direction === 'asc'
+          ? valueA.localeCompare(valueB)
+          : valueB.localeCompare(valueA);
+      }
+
+      if (typeof valueA === 'number' && typeof valueB === 'number') {
+        return direction === 'asc' ? valueA - valueB : valueB - valueA;
+      }
+
+      return 0;
+    });
+  }
 }
